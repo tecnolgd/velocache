4e0f7d383ec80bd2b31a3d61610971ff01a930dc
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2026-02-14 16:25:56 +0000

    feat: modified node structure

--- src/cache.cpp
+++ src/cache.cpp
@@ -3,23 +3,6 @@
 struct Node {
     std::string key;
     std::string value;
-    Node* llink1 = nullptr;
-    Node* rlink = Node1;
-}
-struct Node1 {
-    std::string key;
-    std::string value;
-    Node* rlink2= Node2;
-    Node* llink2 = Node;
-}
-struct Node2 {
-    std::string key;
-    std::string value;
-    Node* llink = Node1 ;
-    Node* rlink= nullptr;
-}
-
-
-for(int i=0; i<3; i++){
-    printf(%d\t, )
-}
+    Node* prev = nullptr;
+    Node* next = nullptr;
+};
