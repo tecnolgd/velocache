1b5da6ab305706de410c91347fc601f943c66758
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2026-02-14 16:22:34 +0000

    experimenting function implementations

---
+++ src/cache.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+struct Node {
+    std::string key;
+    std::string value;
+    Node* llink1 = nullptr;
+    Node* rlink = Node1;
+}
+struct Node1 {
+    std::string key;
+    std::string value;
+    Node* rlink2= Node2;
+    Node* llink2 = Node;
+}
+struct Node2 {
+    std::string key;
+    std::string value;
+    Node* llink = Node1 ;
+    Node* rlink= nullptr;
+}
+
+
+for(int i=0; i<3; i++){
+    printf(%d\t, )
+}
