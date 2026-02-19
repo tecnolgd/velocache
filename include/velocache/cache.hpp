4ceb457e61b808b02282e0031b41e26e492065ec
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2026-02-19 16:16:03 +0000

    feat: added cache.hpp header file

--- include/velocache/cache.hpp
+++ include/velocache/cache.hpp
@@ -1,1 +1,21 @@
+//header file for src/cache.cpp
 
+#ifndef CACHE
+#define CACHE
+
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+struct Node {
+    std::string key;
+    std::string value;
+    Node* prev;
+    Node* next;
+};
+std::string getValue(std::string key);
+void attachToHead(Node* &n);
+void disconnect(Node* n);
+void printData();
+
+#endif
