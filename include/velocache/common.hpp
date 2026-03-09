2fa9e64f7313a1020affb5d620b3e0847858ccd5
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2026-03-09 16:32:59 +0000

    feat: added prototypes involving storage of cache data ( printData(), store_cache_data(Node* current) )

---
+++ include/velocache/common.hpp
@@ -0,0 +1,10 @@
+#ifndef COMMON
+#define COMMON
+
+#include <iostream>
+#include <string>
+
+void printData(); //print cache data form the file
+void store_cache_data(Node* current); //store cache data (persistent data)
+
+#endif
