9945728fb8b5af5d0b211288e7aee4fc28f1588b
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2026-03-14 09:25:38 +0000

    feat: implemented a seperate file for cache server functionalities

---
+++ apps/server.cpp
@@ -0,0 +1,33 @@
+
+#include "../include/velocache/cache.hpp"
+#include "../include/velocache/common.hpp"
+
+
+
+int main(){
+    int number;
+    std::string userName, data;
+
+    load_from_file(); //load stored cache data from the file (hydration)
+
+    std::cout<<"Enter number of users: ";
+    std::cin>>number;
+    for(int i=0; i<number; i++){
+        
+        std::cout<<"Enter username- "<<i+1<<"and its data: ";
+        std::cin>>userName>>data;
+        putValue(userName, data);
+    }
+    store_cache_data(head);
+    printData();
+
+    std::string dataNeeded;
+    std::cout<<"\nEnter key for which data is to be found: ";
+    std::cin>>dataNeeded;
+    std::cout<<"Getting data: "<< getValue(dataNeeded)<<std::endl;
+    store_cache_data(head);
+    printData();
+    
+    //runBenchmark(10000);
+    return 0;
+}
