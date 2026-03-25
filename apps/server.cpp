1d8024d1d505b7e0a33ef9d8d9a9640847ecb505
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2026-03-25 10:07:27 +0000

    feat: multiple sessions as user needs using a simple do-while loop

--- apps/server.cpp
+++ apps/server.cpp
@@ -1,34 +1,57 @@
 //server.cpp --> the cache application
 #include "../include/velocache/cache.hpp"
 #include "../include/velocache/common.hpp"
+#include <cctype>
 
 int main(){
     int number, choice;
     std::string userName, data;
+    std::string dataNeeded;
 
     std::cout<<"VeloCache - high-speed LRU eviction cache >>>>\n";
-        std::cout<<"Enter Operation choice(1 --> use cache; 2 --> display cache contents): ";
-    std::cin>>choice;
-    
-    load_from_file(); //load stored cache data from the file (hydration)
 
-    std::cout<<"Enter number of users: ";
-    std::cin>>number;
-    for(int i=0; i<number; i++){
+    do{
+        std::cout<<"\nOperations supported\n1. Data storage\n2. Data retrieval\n3. Cache display\n4. Exit\n";
+
+        std::cout<<"Enter your choice: ";
+        std::cin>>choice; //error check needed in future versions
         
-        std::cout<<"Enter username- "<<i+1<<"and its data: ";
-        std::cin>>userName>>data;
-        putValue(userName, data);
-    }
-    store_cache_data(head);
-    printData();
+        switch(choice){
+            case 1:
+                load_from_file(); //load stored cache data from the file (hydration)
 
-    std::string dataNeeded;
-    std::cout<<"\nEnter username(key) for which data is to be found: ";
-    std::cin>>dataNeeded;
-    std::cout<<"Getting data: "<< getValue(dataNeeded)<<std::endl;
-    store_cache_data(head);
-    printData();
-    
+                std::cout<<"Enter number of users: ";
+                std::cin>>number;
+                for(int i=0; i<number; i++){
+                    
+                    std::cout<<"Enter username- "<<i+1<<"and its data: ";
+                    std::cin>>userName>>data;
+                    putValue(userName, data);
+                }
+                store_cache_data(head);
+                //printData(); --> optional to map the LRU eviction in action
+                break;
+
+            case 2: 
+                std::cout<<"\nEnter username(key) for which data is to be found: ";
+                std::cin>>dataNeeded;
+                std::cout<<"Getting data: "<< getValue(dataNeeded)<<std::endl;
+                store_cache_data(head);
+                
+                break;
+
+            case 3:
+                std::cout<<std::endl;
+                printData();
+                break;
+
+            case 4: 
+                std::cout<<"Server terminated.\n";
+                exit(0);
+            
+            default: std::cout<<"Invalid choice! Try again.\n";
+        }
+    }while(true);
+        
     return 0;
 }
