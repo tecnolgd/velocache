5994088097aed65c46719099980fe6df107e4924
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2026-05-12 13:52:34 +0000

    chore: added function prototypes of newly added i/p sanitization functions

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,6 +1,11 @@
 #ifndef UTILS
 #define UTILS
 
+#include <string>
+
 int getValidatedChoice(); //function to validate user choice in velocache menu
+std::string getValidatedKeyInput();      //get validated key from user
+std::string getValidatedValueInput();   //get validated value from user
+int getValidatedNumberInput(int maxLimit = 1000); //get validated number from user
 
 #endif
