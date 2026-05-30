91c831e419bc0c8ec1c17d53f83176933a55a23b
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2026-05-30 11:53:39 +0000

    chore: added header for csv export function

--- include/utils.hpp
+++ include/utils.hpp
@@ -2,10 +2,14 @@
 #define UTILS
 
 #include <string>
+#include <limits>
+#include <ios>
 
 int getValidatedChoice(); //function to validate user choice in velocache menu
 std::string getValidatedKeyInput();      //get validated key from user
 std::string getValidatedValueInput();   //get validated value from user
 int getValidatedNumberInput(int maxLimit = 1000); //get validated number from user
+
+void exportMetricsToCSV(const std::string& opName, double p50, double p95, double p99); //exporting benchmark metrics to CSV
 
 #endif
