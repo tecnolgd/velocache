82669c22a5fd5186496a0eb14ad665e631c8eaf8
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2026-07-30 08:23:14 +0000

    chore: modified function prototypes of utility modules

--- include/utils.hpp
+++ include/utils.hpp
@@ -4,6 +4,9 @@
 #include <string>
 #include <limits>
 #include <ios>
+#include <fstream>
+
+class Cache; //class declaration to be used by validate_file()
 
 int getValidatedChoice(); //function to validate user choice in velocache menu
 std::string getValidatedKeyInput();      //get validated key from user
@@ -12,6 +15,8 @@ int getValidatedNumberInput(int maxLimit = 1000); //get validated number from us
 
 void exportMetricsToCSV(const std::string& opName, double p50, double p95, double p99); //exporting benchmark metrics to CSV
 
-bool confirmOverwrite(const std::string& key); //to handle duplicate key inputs
+bool confirmOverwrite(const std::string& key, const Cache &cache); //to handle duplicate key inputs
+
+int validate_file(std::ifstream &fileObj, Cache &cache); // to load valid data from the file
 
 #endif
